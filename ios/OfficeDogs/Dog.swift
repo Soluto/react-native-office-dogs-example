95b66c4edde9672edbf274fc48a5352cb0e472b5
Elada <elada@soluto.com>
2016-07-27 14:23:44 +0300

    Add dog model and display it when chosen in the list

---
+++ ios/OfficeDogs/Dog.swift
@@ -0,0 +1,21 @@
+//
+//  Dog.swift
+//  OfficeDogs
+//
+//  Created by Soluto on 27/07/2016.
+//  Copyright © 2016 elad. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Dog {
+    let name: String
+    let furColor: UIColor
+    let breed: String
+    
+    func asDictionary() -> [String:String] {
+        let colorComponents = CGColorGetComponents(furColor.CGColor)
+        return ["name" : name, "furColor" : "\(colorComponents[0])\(colorComponents[1])\(colorComponents[2])", "breed" : breed]
+    }
+}
--- ios/OfficeDogs/MasterViewController.swift
+++ ios/OfficeDogs/MasterViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    var dogs = [Dog]()
 
 
     override func viewDidLoad() {
@@ -38,7 +38,7 @@ class MasterViewController: UITableViewController {
     }
 
     func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
+        dogs.insert(Dog(name: "django", furColor: UIColor.whiteColor(), breed: "Terrier"), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
@@ -48,9 +48,9 @@ class MasterViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let dog = dogs[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = dog
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -64,14 +64,14 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return dogs.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let dog = dogs[indexPath.row]
+        cell.textLabel!.text = dog.name
         return cell
     }
 
@@ -82,7 +82,7 @@ class MasterViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
+            dogs.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
